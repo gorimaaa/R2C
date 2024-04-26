bddeba2b9f9cfa8f9a00f288dab7879838ede0c4
Zakaria Zabili <zakaria.zabili@outlook.fr>
2024-04-26 15:49:50 +0200

    Navbarre

--- R2C_Services/App.tsx
+++ R2C_Services/App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// Importez les composants de vos différentes pages
+import Page1 from './App/Screens/CompletedScreen/Completed';
+import Page2 from './App/Screens/FormsScreen/Forms';
+import Page3 from './App/Screens/ProfilScreen/Profil';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Page1" component={Page1} />
+        <Tab.Screen name="Page2" component={Page2} />
+        <Tab.Screen name="Page3" component={Page3} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

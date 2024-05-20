14c9a789148ac0211ca0f424fa441722af7c578d
gorimaaa <juba.belhocine@outlook.fr>
2024-05-20 18:03:19 +0200

    modif

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,4 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
-
-module.exports = defaultConfig;
-
-/*const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);*/
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
